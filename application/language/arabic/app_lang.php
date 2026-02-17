a401ba1fb61a6bfba7774738536b48ce66b553f2
mani-mitrah <manikandan.tm@mitrahsoft.in>
2026-02-17 11:45:40 +0530

    Add files via upload

---
+++ application/language/arabic/app_lang.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Arabic language
+ */
+
+/*
+ *    HOME PAGE
+ *
+ */
+$lang['Category Name'] = 'اسم التصنيف';
+$lang['Create'] = 'خلق';
+$lang['View Category'] = 'مشاهدة الفئة';
+$lang['Category'] = 'فئة';
+$lang['Update'] = 'تحديث';
+$lang['Status'] = 'حالة';
+$lang['Category Name'] = 'اسم التصنيف';
+$lang['Category Name'] = 'اسم التصنيف';
+$lang['Category Name'] = 'اسم التصنيف';
+$lang['do']='هل لديك رقم مرجعي طبي';
+$lang['ref_no']='رقم المرجع';
+
+
